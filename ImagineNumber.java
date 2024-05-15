bf52af3cf4a12ce83a002f9f0a66187d4a39da0c
rajeshsvec <119128080+rajeshsvec@users.noreply.github.com>
2024-05-15 22:34:30 +0530

    UpdateImagineNumber.java

--- ImagineNumber.java
+++ ImagineNumber.java
@@ -109,7 +109,7 @@ public static void main(String[] args) {
     System.out.println();
     System.out.println("        HELLO   MATH    WIZARD");
     System.out.println();
-    System.out.println("Imagine a number of range 1-21 in your mind");
+    System.out.println("Imagine a number of range 1-21 in your mind and you need to enter three inputs");
     System.out.println();
     int array[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21};
     print(array);
